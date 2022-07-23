935805248a6a62f7d19979be1e2d36fdd44ab083
Abhinav <abhinavkudnar152@gmail.com>
2022-07-23 11:21:48 +0530

    Making change into proxy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 
-
+//https://reactfrontendabhinav.netlify.app/
 import Signin from "./components/Signin"
 import Signup from "./components/Signup"
 import Showall from "./components/Showall"
